9b1c17a0079153c2b297c54bc0537a1abce29419
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-09-19 20:21:44 -0700

    Artur commit

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -2,7 +2,8 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { clickElement, getText} = require("../../lib/commands.js");
+
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -17,18 +18,50 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
+Given("клиент находится на странице {string}", async function (string) {
+  return await this.page.goto(`http://qamid.tmweb.ru/client${string}`, {
     setTimeout: 20000,
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("клиент выбирает {int}-й день", async function (int) {
+        return await clickElement(this.page, `body > nav > a:nth-child(${int}) > span.page-nav__day-week`);
+});
+When("клиент выбирает время", async function () {
+  return await clickElement(this.page, "[data-seance-id='173']");
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
+When("клиент выбирает {int}-й ряд {int} место", async function (int1, int2) {
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
+  );
+  await clickElement(this.page,".acceptin-button");
+ 
+});
+When("клиент выбирает {int}-й ряд {int},{int} место", async function (int1, int2, int3) {
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
+  );
+  await clickElement(
+    this.page,
+    `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int3})`
+  );
+  await clickElement(this.page,".acceptin-button");
+ 
+});
+
+Then("клиент видит сообщение Вы выбрали билеты:", async function () {
+  const actual = await getText(this.page,"h2");
+    const expected = "Вы выбрали билеты:";
+    expect(actual).contain(expected);
+});
+
+Then("кнопка Забронировать не активна", async function () {
+  const button = await this.page.$eval(
+    ".acceptin-button",
+    (el) => el.disabled
+  );
+  expect(button).toBe(true);
 });
