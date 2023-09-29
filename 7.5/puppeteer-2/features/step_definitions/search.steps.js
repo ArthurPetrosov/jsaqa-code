240134f8bb8028669fce839560441e29034d0042
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-09-29 16:55:37 -0700

    Add Timeout commit

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -5,7 +5,7 @@ const { Given, When, Then, Before, After } = require("cucumber");
 const { clickElement, getText} = require("../../lib/commands.js");
 
 
-Before(async function () {
+Before({timeout: 60 * 1000},async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
   this.browser = browser;
@@ -20,7 +20,7 @@ After(async function () {
 
 Given("клиент находится на странице {string}", async function (string) {
   return await this.page.goto(`http://qamid.tmweb.ru/client${string}`, {
-    setTimeout: 20000,
+    setTimeout: 5000,
   });
 });
 
@@ -31,7 +31,7 @@ When("клиент выбирает время", async function () {
   return await clickElement(this.page, "[data-seance-id='173']");
 });
 
-When("клиент выбирает {int}-й ряд {int} место", async function (int1, int2) {
+When("клиент выбирает {int}-й ряд {int} место",{timeout: 60000}, async function (int1, int2) {
   await clickElement(
     this.page,
     `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
@@ -39,7 +39,7 @@ When("клиент выбирает {int}-й ряд {int} место", async fun
   await clickElement(this.page,".acceptin-button");
  
 });
-When("клиент выбирает {int}-й ряд {int},{int} место", async function (int1, int2, int3) {
+When("клиент выбирает {int}-й ряд {int},{int} место",{timeout: 60000}, async function (int1, int2, int3) {
   await clickElement(
     this.page,
     `div.buying-scheme__wrapper > div:nth-child(${int1}) > span:nth-child(${int2})`
@@ -52,16 +52,17 @@ When("клиент выбирает {int}-й ряд {int},{int} место", asy
  
 });
 
-Then("клиент видит сообщение Вы выбрали билеты:", async function () {
+Then("клиент видит сообщение Вы выбрали билеты:",{timeout: 60000}, async function () {
   const actual = await getText(this.page,"h2");
     const expected = "Вы выбрали билеты:";
     expect(actual).contain(expected);
 });
 
-Then("кнопка Забронировать не активна", async function () {
+Then("кнопка Забронировать не активна",{timeout: 60000}, async function () {
   const button = await this.page.$eval(
     ".acceptin-button",
     (el) => el.disabled
   );
-  expect(button).toBe(true);
+  expect(button).toEqual(true);
+  
 });
